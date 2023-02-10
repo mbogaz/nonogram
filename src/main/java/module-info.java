9f8b7da4aeb2daac30b1b56e602be969823da289
Mahmut <mahmut@codefirst.io>
2023-02-10 17:18:20 +0300

    initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module com.codefirst.nonogram_fx {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.codefirst.nonogram_fx to javafx.fxml;
+    exports com.codefirst.nonogram_fx;
+}
