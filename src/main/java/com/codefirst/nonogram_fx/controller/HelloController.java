78b274ac12b6915b1b44538faefbf6fd5d055d27
Mahmut <mahmut@codefirst.io>
2023-02-10 18:28:58 +0300

    Picture Pixelise Controller

--- src/main/java/com/codefirst/nonogram_fx/controller/HelloController.java
+++ src/main/java/com/codefirst/nonogram_fx/controller/HelloController.java
@@ -1,14 +1,22 @@
 package com.codefirst.nonogram_fx.controller;
 
+import com.codefirst.nonogram_fx.HelloApplication;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
+
+    Stage newStage = new Stage();
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void onHelloButtonClick() throws IOException {
+        FXMLLoader picturePixeliseLoader = new FXMLLoader(HelloApplication.class.getResource("picture-pixelise.fxml"));
+        Scene scene = new Scene(picturePixeliseLoader.load(), 1280, 720);
+        newStage.setScene(scene);
+        newStage.show();
     }
 }
